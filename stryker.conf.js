5fd68b33a9da787baef888a2062616ff3e4ef76b
Jakub Banaś <kuba792@gmail.com>
2018-11-24 20:53:26 +0100

    add stryker script

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,4 +1,4 @@
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     mutator: "javascript",
     packageManager: "yarn",
@@ -7,13 +7,13 @@ module.exports = function (config) {
     transpilers: [],
     coverageAnalysis: "off",
     mutate: [
-      "src/**/*.js", 
-      "!src/**/*.test.js", 
-      "!src/**/index.js", 
+      "src/components/**/*.js",
+      "!src/**/*.test.js",
+      "!src/**/index.js",
       "!src/**/serviceWorker.js"
     ],
     jest: {
-      projectType: 'react',
+      projectType: "react"
     }
   });
 };
